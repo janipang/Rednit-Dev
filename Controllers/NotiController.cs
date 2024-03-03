e62c439cdbdfb6115ea513f9e7b0bc58fff0365a
Herosuit3 <teerawat2441@gmail.com>
2024-03-03 23:28:33 +0700

    create noti

---
+++ Controllers/NotiController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RednitDev.Models;
+
+namespace RednitDev.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
