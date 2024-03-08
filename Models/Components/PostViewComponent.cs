3ed9efa6b4e95424e1608682561e67758771a2bb
Chompoo <97285832+Nattharika-Jeksungnern@users.noreply.github.com>
2024-03-08 13:33:03 +0700

    edit ann to RednitDev

--- Models/Components/PostViewComponent.cs
+++ Models/Components/PostViewComponent.cs
@@ -1,8 +1,8 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
-using ann.Models;
+using RednitDev.Models;
 
-namespace ann.Components{
+namespace RednitDev.Components{
     public class PostViewComponent: ViewComponent
     {
         public async Task<IViewComponentResult> InvokeAsync(Post post)
