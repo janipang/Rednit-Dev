6aa5bf2e00a9d57d607f4539e5d5439c75916b92
Herosuit3 <teerawat2441@gmail.com>
2024-03-14 23:28:44 +0700

    fav adjust

--- Models/Components/FavPostViewComponent.cs
+++ Models/Components/FavPostViewComponent.cs
@@ -6,17 +6,8 @@ using RednitDev.Services;
 namespace RednitDev.Components{
     public class FavPostViewComponent: ViewComponent
     {
-        private ManagerService _Manager;
-        public FavPostViewComponent(ManagerService managerService)
-        {
-            _Manager = managerService;
-        }
-
         public async Task<IViewComponentResult> InvokeAsync(Post post)
         {
-            User user = _Manager.GetUserByUsername(post.Author.Username);
-            Console.WriteLine(user.Profile.Image);
-            ViewBag.ProfileImage = user.Profile.Image;
             return View(post);
         }
     }
