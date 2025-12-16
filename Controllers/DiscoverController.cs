755984f9755d8b05f592061a6ed40fbb8a0f3cf5
janipang <pang.janista@gmail.com>
2025-12-16 17:22:41 +0700

    fix: order last post first

--- Controllers/DiscoverController.cs
+++ Controllers/DiscoverController.cs
@@ -466,6 +466,7 @@ public class DiscoverController : Controller
                 activePosts.Add(post);
             }
         }
+        activePosts.Reverse();
         return activePosts;
     }
 
